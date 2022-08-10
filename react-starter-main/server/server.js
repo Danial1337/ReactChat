308e20876f040634985b3ab11864ef7e95e50061
needtopassclass <danal_123@hotmail.com>
2022-08-10 21:43:22 +0200

    installed WS, 50% setup for WS done

--- react-starter-main/server/server.js
+++ react-starter-main/server/server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import { WebSocketServer } from "ws";
 
 const app = express();
 
 app.use(express.static("../client/dist"));
 
+const wsServer = new WebSocketServer({noServer: true})
 
 const server = app.listen(process.env.PORT || 3000, () =>{
 console.log("Server started on " + server.address().port);
+server.on("upgrade", (req,socket, head) => {
+  wsServer.handleUpgrade(req, socket, header, () =>{});
+});
+
 });
