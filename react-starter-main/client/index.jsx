1c91a4900d67806329c345f10605cf568301fb79
needtopassclass <danal_123@hotmail.com>
2022-08-10 18:29:46 +0200

    Setup for Heroku, as explained in forelesning

--- react-starter-main/client/index.jsx
+++ react-starter-main/client/index.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello Word! :D</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>Hello Word! :D</h1>, document.getElementById("app"));  
--- react-starter-main/server/server.js
+++ react-starter-main/server/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static("../client/dist"));
 
+
 const server = app.listen(process.env.PORT || 3000, () =>{
 console.log("Server started on " + server.address().port);
 });
