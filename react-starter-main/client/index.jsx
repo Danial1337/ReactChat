124859669dd16ca44d0faa7b857a366e31d635bf
needtopassclass <danal_123@hotmail.com>
2022-08-10 18:53:48 +0200

    Added login(not through google yet)

--- react-starter-main/client/index.jsx
+++ react-starter-main/client/index.jsx
@@ -1,4 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello Word! :D</h1>, document.getElementById("app"));  
+
+function Login(props){
+  const [username, setUsername] = useState();
+  function handleSubmit(event){
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+    <h1>Please log in</h1>
+      <form onSubmet={handleSubmit}> 
+        <label>
+          Username: 
+          <input value={username} onChange={e => setUsername(e.target.value)}/>
+          </label>
+          <button>Log in</button>
+      </form>
+    </div>
+  )
+}
+
+function Application(){
+  const [username, setUsername] = useState();
+
+  if(!username){
+    return <Login/>
+
+     //<Login/>
+  }
+}
+
+
+return <div>Hello {username}</div>
+
+
+
+ReactDOM.render(<Application/>, document.getElementById("app"));  
