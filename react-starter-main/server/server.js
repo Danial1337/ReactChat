6829832c11efb703df447546be0423cbb42a2e02
needtopassclass <danal_123@hotmail.com>
2022-08-10 21:50:00 +0200

    websocket now works, its connected

--- react-starter-main/server/server.js
+++ react-starter-main/server/server.js
@@ -6,11 +6,16 @@ const app = express();
 app.use(express.static("../client/dist"));
 
 const wsServer = new WebSocketServer({noServer: true})
+wsServer.on("connect",(socket) => {
+  console.log("ws Connected");
+})
 
 const server = app.listen(process.env.PORT || 3000, () =>{
 console.log("Server started on " + server.address().port);
 server.on("upgrade", (req,socket, head) => {
-  wsServer.handleUpgrade(req, socket, head, () =>{});
+  wsServer.handleUpgrade(req, socket, head, (socket) =>{
+    wsServer.emit("connect", socket, req);
+  });
 });
 
 });
